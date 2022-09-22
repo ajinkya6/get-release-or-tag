afcf0b7527a5eeacc5b45df82e37055c12cd23be
ajinkya6 <112479022+ajinkya6@users.noreply.github.com>
2022-09-22 12:43:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 # Default GITHUB_SHA
 tag="${GITHUB_SHA}"
 
-# If release parse the tag name from GITHUB_REF
-if [ "${GITHUB_EVENT_NAME}" == "release" ]; then
-  ref="${GITHUB_REF}"
-  tag=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
-fi
+# # If release parse the tag name from GITHUB_REF
+# if [ "${GITHUB_EVENT_NAME}" == "release" ]; then
+#   ref="${GITHUB_REF}"
+#   tag=$(echo $GITHUB_REF | sed 's/refs\/tags\///g')
+# fi
 
 echo "::set-output name=tag::$tag"
